9b8d24a85aed628a2ad9b3c4546376fb3e8ca685
Sarah Austin <scaustin34@gmail.com>
2023-03-28 19:26:15 -0400

    add migration, models, and seeds for t11-15

--- db/schema.rb
+++ db/schema.rb
@@ -380,6 +380,46 @@ ActiveRecord::Schema[7.0].define(version: 20_230_328_150_712) do
     t.index ['treatment_setting_code'], name: 'index_treatment_setting_code'
   end
 
+  create_table 'list_co_occurring_statuses', force: :cascade do |t|
+    t.string 'co_occurring_sud_mh_code', null: false
+    t.string 'co_occurring_sud_mh'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['co_occurring_sud_mh_code'], name: 'index_co_occurring_sud_mh_code'
+  end
+
+  create_table 'list_languages', force: :cascade do |t|
+    t.string 'primary_language_code', null: false
+    t.string 'primary_language'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['primary_language_code'], name: 'index_primary_language_code'
+  end
+
+  create_table 'list_marital_statuses', force: :cascade do |t|
+    t.string 'marital_status_code', null: false
+    t.string 'marital_status'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['marital_status_code'], name: 'index_marital_status_code'
+  end
+
+  create_table 'list_referral_sources', force: :cascade do |t|
+    t.string 'referral_source_code', null: false
+    t.string 'referral_source'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['referral_source_code'], name: 'index_referral_source_code'
+  end
+
+  create_table 'list_smi_sed_statuses', force: :cascade do |t|
+    t.string 'smi_sed_code', null: false
+    t.string 'smi_sed'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.index ['smi_sed_code'], name: 'index_smi_sed_code'
+  end
+
   create_table 'master_claims', force: :cascade do |t|
     t.bigint 'master_claim_id', null: false
     t.string 'claim_record_id', null: false

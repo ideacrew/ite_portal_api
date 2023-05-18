1df08f128ea8b97d73cae924cd830ec5e6c42b4e
HARIDHAR YAMJALA <hariym471@gmail.com>
2023-05-18 10:46:26 -0400

    spec updates

--- spec/controllers/claims/master_claims_controller_spec.rb
+++ spec/controllers/claims/master_claims_controller_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 
 RSpec.describe Claims::MasterClaimsController, dbclean: :around_each do
   let!(:claim) do
-    Claims::MasterClaim.create(claim_record_id: '21337382200155227-01',
+    Claims::MasterClaim.create(claim_record_id: '21337382200155227-02',
                                adjudication_date: '2022-01-31',
                                adjudication_status: 'PAID',
                                billing_date: '2022-01-24',
@@ -14,23 +14,23 @@ RSpec.describe Claims::MasterClaimsController, dbclean: :around_each do
                                claim_information_source: 'MMIS RA - Medicaid FFS Claims',
                                date_of_birth: '1957-04-10',
                                first_date_of_service: '2021-12-29',
-                               first_name: 'Daniel',
-                               full_name: 'Daniel L Crawford',
+                               first_name: 'Danie',
+                               full_name: 'Danie L Craw',
                                gender: 'F',
                                procedure_code: 'H0036',
-                               claim_tcn: '21337382200155227')
+                               claim_tcn: '21337382200155228')
   end
 
   context 'GET Index' do
     it 'When user is authorized' do
       expect_any_instance_of(Claims::MasterClaimsController).to receive(:authorize).and_return(true)
-      get :index, params: { search: '21337382200155227' }
+      get :index, params: { search: '21337382200155228' }
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)['claim_count']).to eq 1
     end
 
     it 'When user is not authorized' do
-      get :index, params: { search: '21337382200155227' }
+      get :index, params: { search: '21337382200155228' }
       expect(response.status).to eq 400
       expect(JSON.parse(response.body)['status_text']).to eq 'Unable to authorize the User'
     end
--- spec/controllers/claims/master_clients_controller_spec.rb
+++ spec/controllers/claims/master_clients_controller_spec.rb
@@ -4,28 +4,28 @@ require 'rails_helper'
 
 RSpec.describe Claims::MasterClientsController, dbclean: :around_each do
   let!(:client) do
-    Claims::MasterClient.create(master_client_id: 'M-00000001',
-                                full_name: 'Daniel L Crawford',
-                                first_name: 'Daniel',
+    Claims::MasterClient.create(master_client_id: 'M-00000002',
+                                full_name: 'Dani L Car',
+                                first_name: 'Dani',
                                 middle_name: 'L',
-                                last_name: 'Crawford',
+                                last_name: 'Car',
                                 date_of_birth: Date.today - 30.years,
                                 gender: 'M',
                                 record_source: 'master_claims',
-                                record_source_id: '21337382200155227-01',
+                                record_source_id: '21337382200155227-02',
                                 record_source_date: Date.yesterday)
   end
 
   context 'GET Index' do
     it 'When user is authorized' do
       expect_any_instance_of(Claims::MasterClientsController).to receive(:authorize).and_return(true)
-      get :index, params: { search: 'Daniel L Crawford' }
+      get :index, params: { search: 'Dani L Car' }
       expect(response).to have_http_status(:success)
       expect(JSON.parse(response.body)['client_count']).to eq 1
     end
 
     it 'When user is not authorized' do
-      get :index, params: { search: 'Daniel L Crawford' }
+      get :index, params: { search: 'Dani L Car' }
       expect(response.status).to eq 400
       expect(JSON.parse(response.body)['status_text']).to eq 'Unable to authorize the User'
     end

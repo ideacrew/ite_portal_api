fa76354a5dd2d716f09c84714164d4b30aca6d72
HARIDHAR YAMJALA <hariym471@gmail.com>
2023-10-18 10:13:09 -0400

    remove local auth dependency on azure

--- app/controllers/concerns/secured.rb
+++ app/controllers/concerns/secured.rb
@@ -6,7 +6,10 @@ module Secured
   require 'net/http'
   require 'net/https'
 
+  # rubocop:disable Metrics/PerceivedComplexity
   def authorize
+    return if valid_local_user?
+
     auth_header = request.headers['Authorization']
     if auth_header
       token = auth_header.split(' ')[1]
@@ -33,8 +36,16 @@ module Secured
   rescue StandardError => e
     render json: { status_text: e.to_s, status: 400, content_type: 'application/json' }, status: 400
   end
+  # rubocop:enable Metrics/PerceivedComplexity
 
   private
+
+  def valid_local_user?
+    return false unless Rails.env.development? && params[:local_user_email].present?
+
+    @current_user = check_or_create_user(params[:local_user_email])
+    @current_user.present?
+  end
 
   def check_or_create_user(username)
     existing_user = UserList.where(email: username)&.first
